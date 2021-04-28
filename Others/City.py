534e382e06c6202d21f0a0ac6e5b8e7e3b05db62
Rahul <rahulv0530@gmail.com>
2021-04-28 22:20:27 +0530

    Start Refractor

--- Others/City.py
+++ Others/City.py
@@ -1,9 +1,9 @@
 from random import randint
 from typing import Tuple
 
-
 class City:
 
+    # Random Initialization if not specified
     def __init__(self, x: float = None, y: float = None) -> None:
         self.x = x if (x) else randint(0, 200)
         self.y = y if (y) else randint(0, 200)
--- Others/City_Manager.py
+++ Others/City_Manager.py
@@ -1,22 +1,23 @@
 from typing import List
 from City import City
 
+# Storing the cities data
 cities: List[City] = []
 distances: List[List[float]] = []
 
-
+# Adding cities to the list
 def addCity(city: City) -> None:
     cities.append(city)
 
-
+# Get a particular city
 def getCity(index: int) -> City:
     return cities[index]
 
-
+# Get the total number of cities
 def getLength() -> int:
     return len(cities)
 
-
+# Pre-Calculate distances between each pair of cities
 def calculateDistances() -> None:
     for start in cities:
 
@@ -28,11 +29,11 @@ def calculateDistances() -> None:
 
         distances.append(distances_from_start)
 
-
+# Get distance between two cities
 def getDistance(index_a: int, index_b: int) -> float:
     return distances[index_a][index_b]
 
-
+# Clearing the data
 def clear():
     cities.clear()
     distances.clear()
